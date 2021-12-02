2b5cd4349a84e1bb3a5d46840af2ceeb76959c17
Leo Gomez <dalgomz@hotmail.com>
2021-12-02 20:08:07 +0100

    Integration Phase 1

---
+++ Assets/Script/Navigation/MainLab.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainLab : MonoBehaviour
+{
+    public Button button;
+    public string sceneName = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(changeScene);
+    }
+    void changeScene(){
+        if (sceneName=="lab") SceneHandler.LabScene();
+        if (sceneName=="story") SceneHandler.StoryScene();
+        if (sceneName=="main") SceneHandler.MainMenu();
+    }
+}
---
+++ Assets/Script/Navigation/SceneHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    
+    public static void MainMenu() {  
+        SceneManager.LoadScene("MainMenu");  
+    } 
+    public static void LabScene() {  
+        SceneManager.LoadScene("LabScene");  
+    }  
+    public static void StoryScene() {
+        SceneManager.LoadScene("MapTester");  
+    }
+}
